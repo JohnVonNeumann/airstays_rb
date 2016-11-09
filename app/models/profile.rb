a4ef3c216bfa9eb9492d7f37883150fcaaf7e451
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-09 14:29:45 +1100

    added home controller and linked to profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  #user must enter these required fields 
+  validates :first_name,  presence: true
+  validates :last_name, presence: true
 end
