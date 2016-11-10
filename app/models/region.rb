a89d1181e3edc5ec8aa31f6cd8b7ba98ff5d8a59
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-10 12:10:58 +1100

    rspec test region model

--- app/models/region.rb
+++ app/models/region.rb
@@ -5,11 +5,18 @@ class Region < ApplicationRecord
    def validate_each(record, attribute, value)
      country = ISO3166::Country.new(value)
      # 'value' should be an alpha2 country code
-     record.errors.add attribute, "must be valid alpha 2 country code" unless country.valid?
+     record.errors.add attribute, "must be valid alpha 2 country code" unless country.present? and country.valid?
+     # double check for country not being nil, and also being a valid country code
    end
   end
 
   validates :name, presence: true
   validates :country_code, presence: true, length: { is: 2 }, country_code: true
+  before_save :downcase_country_code
+
+  private
+    def downcase_country_code
+      self.country_code = self.country_code.downcase
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20161109233726) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id", unique: true
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "regions", force: :cascade do |t|
